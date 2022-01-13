7826b955e3ccb0f1b4f3f1b3943b02a406224f94
Martti Malmi <sirius@iki.fi>
2022-01-13 09:54:45 +0200

    close outgoing tx listener only when conn closed

--- src/adapters/websocket_server.rs
+++ src/adapters/websocket_server.rs
@@ -39,7 +39,9 @@ impl Actor for MyWs {
                     }
                     let res = addr.try_send(OutgoingMessage { gun_message: message }); // TODO break on Closed error only
                     if let Err(e) = res {
-                        break;
+                        if let actix::prelude::SendError::Closed(_e) = e {
+                            break;
+                        }
                     }
                 }
             }
